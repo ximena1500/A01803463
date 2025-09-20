7f64fd1957ed86da70a1f875513cf17c574d150e
ximena1500 <a01803463@tec.mx>
2025-09-20 00:07:54 -0600

    Create s14_A01803463.py
    El tercero no me salió

---
+++ s14_A01803463.py
@@ -0,0 +1,27 @@
+#código para saber la edad de un adulto
+
+valida = False
+while not valida :
+  edad = int(input("Cuantos años tienes?"))
+  if edad < 100 and edad > 18 :
+    print("De acuerdo")
+    Valida = True
+
+
+#Código para determinar la edad de un adolescente 
+
+valida = False
+while not valida :
+    edad = int(input("Cuantos años tienes?"))
+    if edad < 19 and edad > 13 :
+        print("Edad validada")
+        valida = True
+
+  #Código para preguntar edad
+
+valida = False
+while not valida :
+    edad = int(input("Introduce tu año de nacimiento"))
+    año_actual : 2025 
+    edad = años_actual - Año_nacimiento
+    print("Tu edad es:", edad, "años.")
