253f20a8d752dcf7733b2c5861becad94f026c5e
ximena1500 <a01803463@tec.mx>
2025-10-07 23:34:36 -0600

    Update s18_a01803463.py

--- s18_a01803463.py
+++ s18_a01803463.py
@@ -47,7 +47,101 @@ for i in range(len(articulos)):
         respuesta = input(f"¿Ya compraste '{articulos[i]}'? (s/n): ").lower()
         if respuesta == "s":
             comprado[i] = True
-print(" Estado actualizado de la lista:")
+print("/n Estado actualizado de la lista:")
 for i in range(len(articulos)):
     estado = "Comprado" if comprado[i] else "Pendiente"
     print(f"  - {articulos[i]}: {estado}")
+
+#mayor menor
+
+num = int(input("¿Cuántos números ingresaras?"))
+numeros = [2, 6, 8, 12, 7, 10]
+
+for i in range(num) :
+    nume = float(input(f"ingrese numero {i + 1}: "))
+    numeros.append(nume)
+
+mayor = may(numeros)
+menor = men(numeros)
+promedio = sum(numeros) / len(numeros)
+
+
+print("/num Resultados:") 
+print(f" numero mayor: {mayor}")
+print(f" numero menor: {menor}")
+print(f" proemdio: {promedio: .2f}")
+
+
+#inventario de productos 
+
+alimentos = ["pollo", "aguacate", "leche", "yogurt", "tortillas"]
+cantidad = [5, 15, 10, 2, 20]
+print("inventario actual:")
+for i in range(len(productos)):
+    print(f" - {alimentos[i]}: {cantidades [i]} unidades")
+
+producto_actualizar = input("/n nombre del producto que debe de actualizarse: ")
+
+if producto_actualizar in alimentos :
+    index = productos.index(producto_actualizar)
+    nueva_cantidad = int(input(f"nueva cantidad para {producto_actualizar}: "))
+
+else:
+    print("producto inexistente.")
+
+print("\n Inventario actualizado:")
+for i in range(len(productos)):
+    print(f"  - {productos[i]}: {cantidades[i]} unidades")
+
+print("\n Productos AGOTADOS:")
+agotados = False
+for i in range(len(productos)):
+    if cantidades[i] == 0:
+        print(f"  - {productos[i]}")
+        agotados = True
+
+if not agotados:
+    print("  (No hay productos agotados)")
+
+t = int(input("¿Cuántas tareas deseas agregar a tu lista? "))
+tareas = ["matematicas", "ingles", "quimica"]
+estado = ["true","false", "false"]
+
+for i in range(n):
+    tarea = input(f"Ingrese la tarea #{i + 1}: ")
+    tareas.append(tarea)
+    estado.append(False) 
+print("\n Tareas pendientes:")
+for i in range(len(tareas)):
+    if not estado[i]:
+        print(f"{i + 1}. {tareas[i]}")
+
+marcar = input("\n¿Deseas marcar alguna tarea como completada? (s/n): ")
+
+if marcar == "s":
+    while True:
+        num = int(input("Ingresa el número de la tarea completada (0 para salir): "))
+        if num == 0:
+            break
+        if 1 <= num <= len(tareas):
+            estado[num - 1] = True
+        else:
+            print("Número inválido.")
+
+print("\n Tareas COMPLETADAS:")
+completadas = False
+for i in range(len(tareas)):
+    if estado[i]:
+        print(f"{tareas[i]}")
+        completadas = True
+if not completadas:
+    print("(Aún no has completado ninguna tarea)")
+
+print("\n Tareas PENDIENTES:")
+pendientes = False
+for i in range(len(tareas)):
+    if not estado[i]:
+        print(f"- {tareas[i]}")
+        pendientes = True
+if not pendientes:
+    print("(No tienes tareas pendientes)")
